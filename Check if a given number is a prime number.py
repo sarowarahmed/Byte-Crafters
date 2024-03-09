d472352cd646df3e4a0a4da6eb658a1831a2aefc
Sarowar Ahmed <sectumsempra.cruciobliviate@gmail.com>
2024-03-09 12:54:59 +0530

    Added python code

---
+++ Check if a given number is a prime number.py
@@ -0,0 +1,12 @@
+#Write a program to print whether a given number is prime number or not.
+
+num = int(input("Enter a number: "))
+if num > 1:
+    for i in range(2, num):
+        if num % i == 0:
+            print(num, "is not a prime number")
+            break
+    else:
+        print(num, "is a prime number")
+else:
+    print(num, "is not a prime number")
