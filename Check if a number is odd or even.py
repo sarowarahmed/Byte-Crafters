25a6f13279d54441a3476279c5dfc655c23ad84d
Sarowar Ahmed <sectumsempra.cruciobliviate@gmail.com>
2024-05-27 18:34:02 +0530

    Added python code

---
+++ Check if a number is odd or even.py
@@ -0,0 +1,7 @@
+#Write a program that will tell whether the number entered by the user is odd or even.
+
+num = int(input("Enter a number: "))
+if num % 2 == 0:
+    print("Even")
+else:
+    print("Odd")
