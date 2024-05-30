37e5b36247f5d0ed587b21872f3b0967bd4f3496
Sarowar Ahmed <sectumsempra.cruciobliviate@gmail.com>
2024-05-30 16:00:07 +0530

    Added python code

---
+++ Student's records.py
@@ -0,0 +1,15 @@
+#Input Format
+#The first line contains the integer , the number of students' records. The next  lines contain the names and marks obtained by a student, each value separated by a space. The final line contains query_name, the name of a student to query.
+
+
+#Output Format
+#Print one line: The average of the marks obtained by the particular student correct to 2 decimal places.
+
+if __name__ == '__main__':
+    n = int(input())
+    student_marks = {}
+    for _ in range(n):
+        name, *line = input().split()
+        scores = list(map(float, line))
+        student_marks[name] = scores
+    query_name = input()
