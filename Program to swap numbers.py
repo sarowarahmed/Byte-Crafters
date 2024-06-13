52d776f22578776452afa2c8fb13893a2222b887
Sarowar Ahmed <sectumsempra.cruciobliviate@gmail.com>
2024-06-13 19:52:13 +0530

    Added python code

---
+++ Program to swap numbers.py
@@ -0,0 +1,10 @@
+#Write a program that will swap numbers.
+
+num1 = int(input("Enter first number: "))
+num2 = int(input("Enter second number: "))
+
+num1, num2 = num2, num1
+
+print("After swapping:")
+print("First number:", num1)
+print("Second number:", num2)
