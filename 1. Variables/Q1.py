89d41e785bef4de7ca1ee197ab845ead080ac31e
Sarowar / <113256659+sarowarahmed@users.noreply.github.com>
2024-06-21 13:40:29 +0530

    Delete 1. Variables directory

--- 1. Variables/Q1.py
+++
@@ -1,7 +0,0 @@
-#Store the principal amount, rate of interest, and time in different variables and then calculate the Simple Interest for 3 years.
-
-principal_amount = 234
-rate_of_interest = 6.7
-time = 3
-simple_interest = (principal_amount * rate_of_interest * time)/100
-print (simple_interest)
